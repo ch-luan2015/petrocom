6fe75130f83c3d91ff112dca744a67048bb68f5a
ch-luan2015 <45224298+ch-luan2015@users.noreply.github.com>
2021-01-13 19:12:33 +0700

    propstype

--- src/Views/Home/CarouselHome/CarouselHome.jsx
+++ src/Views/Home/CarouselHome/CarouselHome.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import "./CarouselHome.scss"
 import Slider from "react-slick";
+import PropTypes from 'prop-types';
 
 
 
-const CarouselHome = () => {
+const CarouselHome = ({ arrayImage, settings }) => {
 
-  const imgCarousel = [
-    { name: "1banner.jpg" },
-    { name: "123.jpg" },
-    { name: "b-003.jpg" },
-    { name: "banner01.jpg" },
-    { name: "banner02.jpg" },
-  ]
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+  // const imgCarousel = [
+  //   { name: "1banner.jpg" },
+  //   { name: "123.jpg" },
+  //   { name: "b-003.jpg" },
+  //   { name: "banner01.jpg" },
+  //   { name: "banner02.jpg" },
+  // ]
+  // var settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1
+  // };
 
   const renderCarousel = () => {
-    return imgCarousel.map((item) => {
+    return arrayImage.map((item) => {
       return <div key={item.name}>
         <h3><img src={`/image/Banner/${item.name}`} style={{ objectFit: "cover", width: "100%" }} /></h3>
       </div>
@@ -34,6 +35,12 @@ const CarouselHome = () => {
       {renderCarousel()}
     </Slider>
   );
+};
+
+
+CarouselHome.propTypes = {
+  arrayImage: PropTypes.array.isRequired,
+  settings: PropTypes.data.isRequired
 };
 
 export default CarouselHome;
--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -4,13 +4,27 @@ import HeaderHome from "./HeaderHome/HeaderHome"
 import CarouselHome from './CarouselHome/CarouselHome';
 
 
+const imgCarousel = [
+  { name: "1banner.jpg" },
+  { name: "123.jpg" },
+  { name: "b-003.jpg" },
+  { name: "banner01.jpg" },
+  { name: "banner02.jpg" },
+]
 
+var settingsBanner = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
 const Home = () => {
 
   return (
     <>
       <HeaderHome />
-      <CarouselHome />
+      <CarouselHome arrayImage={imgCarousel} settings={settingsBanner} />
     </>
   );
 };
